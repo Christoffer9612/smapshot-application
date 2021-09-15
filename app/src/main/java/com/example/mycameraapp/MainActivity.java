6ecec281a05f02e3466a62334896ee8893e06170
Christoffer9612 <stoffekarlsson96@hotmail.com>
2021-09-15 11:10:54 +0200

    Added azmiuth, pitch and roll tracker

--- app/src/main/java/com/example/mycameraapp/MainActivity.java
+++ app/src/main/java/com/example/mycameraapp/MainActivity.java
@@ -7,11 +7,32 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    public static int index = 0;
+    public final String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera/";
+
     private TextView textViewToDisplayRotation;
     private float[] acc = new float[3];
     private float[] mags = new float[3];
@@ -64,7 +85,37 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         textViewToDisplayRotation = findViewById(R.id.textViewToDisplayRotation);
+
+
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA, WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build() );
+
     }
+
+    public void CameraButton(View view) {
+
+        index++;
+        String file = directory + index + ".jpg";
+        File newFile = new File(file);
+        try {
+            newFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Uri outputFileUri = Uri.fromFile(newFile);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivity(cameraIntent);
+
+    }
+
+
+
+
 
     @Override
     protected void onResume()
