03e3dda07eb89e09dff59415f71a5cdf07299355
Christoffer9612 <stoffekarlsson96@hotmail.com>
2021-10-12 11:41:29 +0200

    the accuracy calculated and display as a mean percentage accuracy of the three angles

--- app/src/main/java/com/example/mycameraapp/PostPicActivity.java
+++ app/src/main/java/com/example/mycameraapp/PostPicActivity.java
@@ -13,7 +13,8 @@ import org.json.JSONException;
 
 public class PostPicActivity extends MainActivity { //AppCompatActivity
     private Button btnGoBack;
-    private TextView newAzimuth, newTilt, newRoll, success;
+    private TextView newAzimuth, newTilt, newRoll, success, percentage_error;
+    public int error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,10 +77,22 @@ public class PostPicActivity extends MainActivity { //AppCompatActivity
         newRoll.setText("new roll: " + String.valueOf(ro));
         newRoll.setTypeface(montserrat_medium);
 
+        percentage_error = findViewById(R.id.percentage_error);
+        percentage_error.setTextColor(Color.parseColor("#444444"));
+
+
+        try {
+            percentage_error.setText("Accuracy: " + percentage_error(az, ti, ro) + "%");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        percentage_error.setTypeface(montserrat_medium);
+
         btnGoBack = (Button) findViewById(R.id.button);
         btnGoBack.setBackgroundColor(Color.parseColor("#E2E2E2"));
         btnGoBack.setTextColor(Color.parseColor("#444444"));
         btnGoBack.setTypeface(montserrat_medium);
+
 
         btnGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,6 +100,51 @@ public class PostPicActivity extends MainActivity { //AppCompatActivity
                 openMainActivity(v);
             }
         });
+    }
+
+    public float percentage_error(float az, float ti, float ro) throws JSONException {
+
+        float float_error;
+        float float_error1;
+        float float_error2;
+
+
+        StringBuilder test = findValue(jsonObj, "azimuth");
+        String test2 = test.toString();
+        String[] parts = test2.split(" ");
+        String string_azimuth = parts[3];
+        double d = Double.parseDouble(string_azimuth);
+        float azimuth_float = (float) d;
+
+        StringBuilder test3 = findValue(jsonObj, "tilt");
+        String test4 = test3.toString();
+        String[] parts1 = test4.split(" ");
+        String string_tilt = parts1[3];
+        double d1 = Double.parseDouble(string_tilt);
+        float tilt_float = (float) d1;
+
+        StringBuilder test5 = findValue(jsonObj, "roll");
+        String test6 = test5.toString();
+        String[] parts2 = test6.split(" ");
+        String string_roll = parts2[3];
+        double d3 = Double.parseDouble(string_roll);
+        float roll_float = (float) d3;
+
+        float_error = Math.abs(azimuth_float - az);
+        float_error = azimuth_float - float_error;
+        float_error = (float_error/azimuth_float) * 100;
+
+        float_error1 = Math.abs(tilt_float - ti);
+        float_error1 = tilt_float - float_error1;
+        float_error1 = (float_error1/tilt_float) * 100;
+
+        float_error2 = Math.abs(roll_float - ro);
+        float_error2 = roll_float - float_error2;
+        float_error2 = (float_error2/roll_float) * 100;
+
+
+        float_error = (float_error + float_error1 + float_error2)/3;
+        return Math.round(float_error);
     }
 
     public void openMainActivity(View view) {
