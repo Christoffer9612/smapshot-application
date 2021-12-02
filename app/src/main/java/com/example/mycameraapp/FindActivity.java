78df12f72c0d8b479036af24ba7c61571f4b923a
Alfred Hirschfeld <alfred@hirschfeld.se>
2021-12-02 16:17:15 +0100

    Added dummy checkboxes

--- app/src/main/java/com/example/mycameraapp/FindActivity.java
+++ app/src/main/java/com/example/mycameraapp/FindActivity.java
@@ -3,6 +3,7 @@ package com.example.mycameraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +11,31 @@ import android.widget.CheckBox;
 
 public class FindActivity extends AppCompatActivity {
 
-    private Button btnMain;
+    private Button btnMain, btnFind;
+    private CheckBox checkboxCities, checkboxLandscapes, checkboxMonuments, checkboxRivers, checkboxViewpoints;
+    private Utils utils = new Utils(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
         btnMain = findViewById(R.id.btnMain);
+        btnFind = findViewById(R.id.btnFind);
+        checkboxCities = findViewById(R.id.checkboxCities);
+        checkboxLandscapes = findViewById(R.id.checkboxLandscapes);
+        checkboxMonuments = findViewById(R.id.checkboxMonuments);
+        checkboxRivers = findViewById(R.id.checkboxRivers);
+        checkboxViewpoints = findViewById(R.id.checkboxViewpoints);
+
+        Typeface montserrat_medium = Typeface.createFromAsset(getAssets(), "fonts/montserrat_medium.ttf");
+
+        utils.setButton(btnMain, montserrat_medium);
+        utils.setButton(btnFind, montserrat_medium);
+        utils.setText(checkboxCities, montserrat_medium);
+        utils.setText(checkboxLandscapes, montserrat_medium);
+        utils.setText(checkboxMonuments, montserrat_medium);
+        utils.setText(checkboxRivers, montserrat_medium);
+        utils.setText(checkboxViewpoints, montserrat_medium);
 
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
